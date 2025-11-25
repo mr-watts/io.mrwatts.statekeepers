306f8db92662e7bf2283300a7a9694d70d849681
NoTuxNoBux <tom.gerrits@mrwatts.io>
2025-11-25 10:23:12 +0100

    FEAT: Add RegisterTypedStateKeeper overload with activator

--- Assets/StateKeepers/CHANGELOG.md
+++ Assets/StateKeepers/CHANGELOG.md
@@ -1,2 +1,5 @@
+# Unreleased | Unreleased
+- Add `RegisterTypedStateKeeper` overload accepting activator to allow basing initial value off container services.
+
 # 2025-02-06 | 1.0.0
  - Split up scripts from the `mrwatts.internal.utilities` package
--- Assets/StateKeepers/Runtime/Scripts/Extensions/ContainerBuilderExtensions.cs
+++ Assets/StateKeepers/Runtime/Scripts/Extensions/ContainerBuilderExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using Autofac;
+using Autofac.Core;
 
 namespace MrWatts.Internal.StateKeepers
 {
@@ -17,6 +19,21 @@ namespace MrWatts.Internal.StateKeepers
                 .AsSelf()
                 .SingleInstance()
                 .OnActivating(args => args.Instance.State = initialValue);
+        }
+
+        public static void RegisterTypedStateKeeper<T>(this ContainerBuilder builder, Action<IActivatingEventArgs<EventEmittingStateKeeper<T>>> activator)
+        {
+            builder
+                .RegisterType<StateKeeper<T>>()
+                .AsSelf()
+                .SingleInstance();
+
+            builder
+                .Register(b => new EventEmittingStateKeeper<T>(b.Resolve<StateKeeper<T>>()))
+                .As<IStateKeeper<T>>()
+                .AsSelf()
+                .SingleInstance()
+                .OnActivating(activator);
         }
     }
 }
